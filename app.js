3074c32447fa6a1cc7430b77e12631c906b1c9c0
JamelxD <akjamesworks@hotmail.co.uk>
2020-01-01 14:58:55 +0000

    Removed some code that was used for server test purposes

--- app.js
+++ app.js
@@ -4,23 +4,22 @@ const request = require('request');
 const app = express();
 const PORT = 3000;
 
-app.get('/', function (req, res) {
-    res.send('Hello Worldy');
-})
-
-app.get('/hello', function (req, res) { requestMade(res); })
 app.get('/getLocation', function (req, res) {
     let lat = req.query.lat;
     let long = req.query.long;
     request('https://api.postcodes.io/postcodes?lon=' + long + '&lat=' + lat + '&limit=1', function (error, response, body) {
+        console.log('/getLocation was called');
         if (!error && response.statusCode == 200) {
             var response = JSON.parse(body);
             response = response.result;
 
-            if (response !== null || typeof response !== 'undefined') {
+            if (response !== null && typeof response !== 'undefined') {
                 const borough = response[0]['admin_district'];
                 res.json({borough});
+                return;
             }
+
+            res.status(500).send('Location is not in the United Kingdom.')
         } else {
             console.log(error);
             res.json({error});
@@ -29,7 +28,3 @@ app.get('/getLocation', function (req, res) {
 })
 
 app.listen(PORT, () => console.log(`Express server currently running on port ${PORT}`));
-
-function requestMade(res) {
-    res.send('Request was made successfully');
-}
