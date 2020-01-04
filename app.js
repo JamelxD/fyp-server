f9268b259175646645cc45d93f985df1008df089
JamelxD <akjamesworks@hotmail.co.uk>
2020-01-04 18:51:00 +0000

    Connected DynamoDB

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const request = require('request');
+const AWS = require("aws-sdk");
 
 const app = express();
 const PORT = 3000;
 
+AWS.config.update({region: 'eu-west-1'});
+AWS.config.getCredentials(function(err) {
+    if (err) console.log(err.stack);
+});
+
+var ddb = new AWS.DynamoDB({apiVersion: 'latest'});
 app.get('/getLocation', function (req, res) {
     let lat = req.query.lat;
     let long = req.query.long;
@@ -25,6 +32,27 @@ app.get('/getLocation', function (req, res) {
             res.json({error});
         }
     })
-})
+});
+
+app.get('/getBoroughInfo', function (req, res) {
+    let borough = req.query.borough;
+    var params = {
+        TableName: 'borough_info',
+        Key: {
+            'borough_id': { N: borough }
+        },
+        AttributesToGet: [
+            'disabled_badge_parking_limit',
+        ],
+    };
+
+    ddb.getItem(params, function(err, data) {
+        if (err) {
+            console.log("Error", err);
+        } else {
+            res.json({data});
+        }
+    });
+});
 
 app.listen(PORT, () => console.log(`Express server currently running on port ${PORT}`));
